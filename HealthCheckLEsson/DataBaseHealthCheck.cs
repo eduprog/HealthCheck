c884cb628aa74bf2115f6b7da0eb9f3bfc5ed8db
eduprog <eduprog@gmail.com>
2023-08-04 13:52:07 -0300

    Iniciando repositório

---
+++ HealthCheckLEsson/DataBaseHealthCheck.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthCheckLEsson;
+
+public class DataBaseHealthCheck : IHealthCheck
+{
+    private readonly ApplicationDbContext _context;
+
+    public DataBaseHealthCheck(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await _context.Health.AnyAsync();
+            return HealthCheckResult.Healthy("Banco de dados está acessível e a tabela foi consultada com sucesso.");
+        }
+        catch (Exception ex)
+        {
+            return HealthCheckResult.Unhealthy("Falha ao acessar o banco de dados ou consultar a tabela.", ex);
+        }
+    }
+}
---
+++ HealthCheckLEsson/Health.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HealthCheckLEsson;
+[Table("health")]
+public class Health
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    public string Nome { get; set; }
+
+
+}
